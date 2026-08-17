1bc80ce64263d19d564f1f8912bf72e61d3f15f3
FDGSHOP <gamehacknum@gmail.com>
2026-08-17 10:22:56 +0700

    Create server.js

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch(err => console.error(err));
+
+app.get('/', (req, res) => res.send("Freedom Backend is Running!"));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
